080c53d5eba0d1a5c18615fb4b0a4c3911f9d77c
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2024-01-11 18:06:05 +0100

    feat: add @grzeg95/angular-lib-theme-selector

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideThemeSelector } from '@grzeg95/angular-lib-theme-selector';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideThemeSelector({
+      themes: {
+        names: ['basic'],
+        default: 'basic'
+      }
+    })
+  ]
 };
