121a2f4bd06aada621916b20b433fbe08534d54c
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2024-01-13 01:02:08 +0100

    fix: removed unnecessary packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideThemeSelector } from '@grzeg95/angular-lib-theme-selector';
-
-import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
     provideThemeSelector({
       themes: {
         names: ['basic'],
